b7cfaa24426cdb1e61b293999423025b951d9271
clam3154 <clam3154@bths.edu>
2018-12-10 10:15:59 -0500

    so far

--- src/Deck.java
+++ src/Deck.java
@@ -1,2 +1,36 @@
-public class Deck {
+import java.util.ArrayList;
+
+public class Deck
+{
+    ArrayList<Card> unDealt;
+    ArrayList<Card> Dealt;
+
+    public Deck(String rank, String suit, int pointValue)
+    {
+
+    }
+
+    public boolean isEmpty()
+    {
+        return true;
+    }
+
+    public int size()
+    {
+        int num = 0;
+        return num;
+    }
+
+    public deal()
+    {
+
+    }
+
+    public shuffle()
+    {
+        for(int k = 51; k > 1; k--)
+        {
+            
+        }
+    }
 }
