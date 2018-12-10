6b03adf0fca962f48469c99d108a4e86fefa7724
BT_1N3_12@bktech.local <BT_1N3_12@bktech.local>
2018-12-10 10:08:27 -0500

    Initial commit

---
+++ src/Card.java
@@ -0,0 +1,42 @@
+public class Card
+{
+    private String rank;
+    private String suit;
+    private int pointValue;
+
+    public Card(String rank, String suit, int pointValue)
+    {
+        this.rank = rank;
+        this.suit = suit;
+        this.pointValue = pointValue;
+    }
+
+    public String getRank()
+    {
+        return rank;
+    }
+
+    public String getSuit()
+    {
+        return suit;
+    }
+
+    public int getPointValue()
+    {
+        return pointValue;
+    }
+
+    public boolean equals(Card otherCard)
+    {
+        if(otherCard.getPointValue() == this.pointValue)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public String toString()
+    {
+        return getRank() + ", " + getSuit();
+    }
+}
